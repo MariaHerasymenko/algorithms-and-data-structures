be4d4fa343838c042ec318809f04eb311c6c058f
MariiaGerasimenko <58401712+MariiaGerasimenko@users.noreply.github.com>
2020-03-16 13:59:00 +0200

    Add files via upload

---
+++ 8.1/user.py
@@ -0,0 +1,28 @@
+N = 1000000
+
+def sort(array):
+    if len(array) > 1:
+        mid = len(array) // 2
+        lefthalf = array[:mid]
+        righthalf = array[mid:]
+        sort(lefthalf)
+        sort(righthalf)
+        i = 0
+        j = 0
+        k = 0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                array[k] = lefthalf[i]
+                i += 1
+            else:
+                array[k] = righthalf[j]
+                j += 1
+            k += 1
+        while i < len(lefthalf):
+            array[k] = lefthalf[i]
+            i += 1
+            k += 1
+        while j < len(righthalf):
+            array[k] = righthalf[j]
+            j += 1
+            k += 1
